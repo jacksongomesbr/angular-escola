8b7e9471dc0fc374536a0f2077e097535fa4263c
Jackson Gomes <jgomes@ceulp.edu.br>
2017-11-17 11:15:47 -0300

    Criando módulos publico e admin

---
+++ src/app/publico/publico.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublicoRoutingModule } from './publico-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PublicoRoutingModule
+  ],
+  declarations: []
+})
+export class PublicoModule { }
