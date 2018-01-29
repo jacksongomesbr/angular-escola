53380db8a90cc6bdc15bad7e6c8f1a5372b53ac9
Jackson Gomes <jgomes@ceulp.edu.br>
2018-01-29 12:08:56 -0300

    Atualizando para Angular 5.2.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularEscolaNovo
+# AngularEscola
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.6.
 
 ## Development server
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('angular-escola-novo App', () => {
+describe('angular-escola App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('angular-escola-novo App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Angular!');
   });
 });
