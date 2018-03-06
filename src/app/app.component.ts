1a4a4baf737bf51ba544f7b94205aaac71c7fd24
Leonardo Martins <leodms789@gmail.com>
2018-03-06 15:42:21 -0300

    Prova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,17 @@ export class AppComponent {
   nome = null;
   id = null;
   descricao = null;
+  cargaHoraria = null;
   apresentar = false;
   disciplinas = [
-    new Disciplina(1,'Língua Portuguesa', 'O objetivo norteador da BNCC de ' +
+    new Disciplina(1,'Língua Portuguesa',44, 'O objetivo norteador da BNCC de ' +
       'Língua Portuguesa é garantir a todos os alunos o acesso aos saberes ' +
       'linguísticos necessários para a participação social e o exercício da ' +
       'cidadania, pois é por meio da língua que o ser ' +
       'humano pensa, comunica-se, tem acesso à informação, expressa e ' +
       'defende pontos de vista, partilha ou constrói visões de mundo e ' +
       'produz conhecimento.'),
-    new Disciplina(2,'Educação Física', 'A Educação Física é o componente ' +
+    new Disciplina(2,'Educação Física',32, 'A Educação Física é o componente ' +
       'curricular que tematiza as práticas corporais em suas diversas formas ' +
       'de codificação e significação social, entendidas como manifestações ' +
       'das possibilidades expressivas dos sujeitos e patrimônio cultural ' +
@@ -29,7 +30,7 @@ export class AppComponent {
       'um deslocamento espaço-temporal de um segmento corporal ' +
       'ou de um corpo todo. Logo, as práticas corporais são textos culturais ' +
       'passíveis de leitura e produção.'),
-    new Disciplina(3,'Inglês', 'Aprender a língua inglesa propicia a criação ' +
+    new Disciplina(3,'Inglês',25, 'Aprender a língua inglesa propicia a criação ' +
       'de novas formas de engajamento e participação dos alunos em um mundo ' +
       'social cada vez mais globalizado e plural, em que as fronteiras ' +
       'entre países e interesses pessoais, locais, regionais, nacionais ' +
@@ -41,7 +42,7 @@ export class AppComponent {
       'formativo que inscreve a aprendizagem de inglês em uma perspectiva ' +
       'de educação linguística, consciente e crítica, na qual as dimensões ' +
       'pedagógicas e políticas são intrinsecamente ligadas.'),
-    new Disciplina(4,'Matemática', 'No Ensino Fundamental, essa área, por ' +
+    new Disciplina(4,'Matemática',26, 'No Ensino Fundamental, essa área, por ' +
       'meio da articulação de seus diversos campos – Aritmética, Álgebra, ' +
       'Geometria, Estatística e Probabilidade – precisa garantir que os ' +
       'alunos relacionem observações empíricas do mundo real a ' +
@@ -55,7 +56,7 @@ export class AppComponent {
       'algumas propriedades e a verificação de conjecturas, a partir ' +
       'de outras, podem ser estimuladas, sobretudo ao final do ' +
       'Ensino Fundamental.'),
-    new Disciplina(5,'Ciências', 'Ao estudar Ciências, as pessoas aprendem ' +
+    new Disciplina(5,'Ciências',80, 'Ao estudar Ciências, as pessoas aprendem ' +
       'a respeito de si mesmas, da diversidade e dos processos de evolução ' +
       'e manutenção da vida, do mundo material – com os seus recursos ' +
       'naturais, suas transformações e fontes de energia –, do nosso ' +
@@ -69,19 +70,29 @@ export class AppComponent {
     this.apresentar=true;
   }
 
+  total(){
+    var i = 0;
+    for(let contador of this.disciplinas){
+      i = i + contador.cargaHoraria;
+    }
+    return(i);
+  }
+
   salvar() {
     if (this.editando) {
       this.editando.id = this.id;
       this.editando.nome = this.nome;
+      this.editando.cargaHoraria = this.cargaHoraria;
       this.editando.descricao = this.descricao;
     } else {
-      const d = new Disciplina(this.id,this.nome,this.descricao);
+      const d = new Disciplina(this.id, this.nome, this.cargaHoraria, this.descricao);
       this.disciplinas.push(d);
     }
     this.id = null;
     this.nome = null;
     this.descricao = null;
     this.editando = null;
+    this.cargaHoraria = null;
     this.apresentar = false;
   }
 
@@ -102,6 +113,7 @@ export class AppComponent {
     this.apresentar= true;
     this.id = disciplina.id;
     this.nome = disciplina.nome;
+    this.cargaHoraria = disciplina.cargaHoraria;
     this.descricao = disciplina.descricao;
     this.editando = disciplina;
   }
@@ -109,6 +121,7 @@ export class AppComponent {
   cancelar() {
     this.id = null;
     this.nome = null;
+    this.cargaHoraria = null;
     this.descricao = null;
     this.editando = null;
     this.apresentar = false;
